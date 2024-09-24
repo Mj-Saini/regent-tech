b391d57688648e29f592c0c91084cfb6f9e5f1d4
Mj-Saini <119838410+Mj-Saini@users.noreply.github.com>
2024-09-24 13:07:37 +0530

    undo link changes

--- src/app/components/CommonBtn.jsx
+++ src/app/components/CommonBtn.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const CommonBtn = ({ btnName, btnCss, btn2Styling, btn1Styling, aboutBtn }) => {
   return (
-    <a href="upi://pay?pa=MUNGIPA10.09@cmsidfc&pn=&mc=&tid=&am=1.0&cu=INR&tn="
+    <button 
       className={`px-3 md:px-9 py-2 md:py-[15px] font-medium capitalize border hover:shadow-custom-shadow duration-300 ${btnCss} ${btn1Styling} ${btn2Styling} ${aboutBtn}`}
     >
       {btnName}
-    </a>
+    </button>
   );
 };
 
