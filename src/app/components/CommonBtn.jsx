fd3aee0665cb08763691203e5ad94a9757eaffac
Mj-Saini <119838410+Mj-Saini@users.noreply.github.com>
2024-09-24 12:49:47 +0530

    change link

--- src/app/components/CommonBtn.jsx
+++ src/app/components/CommonBtn.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CommonBtn = ({ btnName, btnCss, btn2Styling, btn1Styling, aboutBtn }) => {
   return (
-    <a href="paytmmp://cash_wallet?pa=326481789425877@cnrb&pn=pay&tn=FNON&am=3.00&cu=INR&mc=4468&url=&mode=02&purpose=00&orgid=159002&sign=By0nrjXvFAIBBcaI==&featuretype=money_transfer"
+    <a href="upi://pay?pa='MUNGIPA10.09@cmsidfc'&pn=''&mc=&tid=''&am=1.0&cu=INR&tn=''"
       className={`px-3 md:px-9 py-2 md:py-[15px] font-medium capitalize border hover:shadow-custom-shadow duration-300 ${btnCss} ${btn1Styling} ${btn2Styling} ${aboutBtn}`}
     >
       {btnName}
